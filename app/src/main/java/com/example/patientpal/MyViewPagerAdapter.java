9f539a7d80dca92d643fd7fd9ec1c062d9e4ef25
madhavi chaudhary <chaudharymadhavi184@gmail.com>
2024-09-22 11:38:19 +0530

    patientpal

---
+++ app/src/main/java/com/example/patientpal/MyViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.patientpal;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.patientpal.fragments.AboutUsFragment;
+import com.example.patientpal.fragments.HomeFragment;
+import com.example.patientpal.fragments.ServiceFragment;
+
+public class MyViewPagerAdapter extends FragmentStateAdapter {
+
+    public MyViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new AboutUsFragment();
+            case 2:
+                return new ServiceFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/com/example/patientpal/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.patientpal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    Button signin,signup;
+    Intent i;
+    @Override
+    protected void onCreate(Bundle SavedInstantState){
+
+        super.onCreate(SavedInstantState);
+        setContentView(R.layout.activity_welcome);
+
+        signin=(Button) findViewById(R.id.btnSignIn);
+        signup=(Button)findViewById(R.id.btnSignUp);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                i=new Intent(WelcomeActivity.this,RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+        signin.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                i=new Intent(WelcomeActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+
+}
